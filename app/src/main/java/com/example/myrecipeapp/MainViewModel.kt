413ebd59e800ac86d68fa399937602ac03f13521
Abhishek123540 <abhidas977@gmail.com>
2024-06-20 13:43:50 +0530

    master

---
+++ app/src/main/java/com/example/myrecipeapp/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.myrecipeapp
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+    data class RecipeState(
+        val loading: Boolean = true,
+        val list: List<Category> = emptyList(),
+        val error: String? = null
+    )
+}
